85e60c633a2f6a7f211cf5bee47cadb1efa60d22
pdione <dione.pape@gmail.com>
2020-10-25 19:24:42 +0100

    :sparkles: Using assertJ

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.condition.*;
 
 import java.time.Duration;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
@@ -27,6 +28,7 @@ class IndexControllerTest {
         assertEquals("index", controller.index(), "wrong view Returned");
         assertEquals("index", controller.index(), ()-> "Another expensive message "+
                 "Make me only if you have to");
+        assertThat(controller.index()).isEqualTo("index");
     }
 
     @Test
